e64835ea0dbc56832d10a8422e128b3c385a052f
Flavio Silva <flsilva7@gmail.com>
2024-02-06 16:57:46 -0300

    upgrade to next 14.1.1-canary.37

--- app/BugDescription.tsx
+++ app/BugDescription.tsx
@@ -4,7 +4,7 @@ export default function BugDescription() {
       <p className="mt-4">
         This bug report is using{" "}
         <span className="font-mono font-semibold">
-          Next.js v14.1.1-canary.5
+          Next.js v14.1.1-canary.37
         </span>
       </p>
       <p className="mt-4">
